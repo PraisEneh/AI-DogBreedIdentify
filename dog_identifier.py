96144570908171bafc10e6b6b159d33389e1f0a7
Praise Chinedu-Eneh <92395785+PraisEneh@users.noreply.github.com>
2022-04-05 21:18:38 -0500

    Add files via upload

--- dog_identifier.py
+++ dog_identifier.py
@@ -2,7 +2,7 @@ import random
 from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import sparse_categorical_crossentropy, binary_crossentropy
+from tensorflow.keras.losses import sparse_categorical_crossentropy, binary_crossentropy, categorical_crossentropy, SparseCategoricalCrossentropy
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -12,7 +12,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.initializers import VarianceScaling
 from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras import backend as k
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 DATADIR = "A:/python/PyCharmProjects/pythonProject/big_data/training_images"
 CATEGORIES = ['bloodhound', 'golden_retriever', 'maltese']
@@ -22,6 +23,8 @@ IMG_SIZE = 64
 training_data = []
 now = datetime.now()
 
+print(len(tf.config.list_physical_devices('GPU')))
+
 def create_training_data():
     for category in CATEGORIES:
         path = os.path.join(DATADIR, category)
@@ -30,6 +33,7 @@ def create_training_data():
             try:
                 img_array = cv2.imread(os.path.join(path, img))
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                new_array = np.array(new_array)
                 training_data.append([new_array, class_num])
             except Exception as e:
                 pass
@@ -44,30 +48,48 @@ y = []
 for features, label in training_data:
     X.append(features)
     y.append(label)
-X = np.array(X)
-X.reshape((X.shape[0], -1))
-#X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-X = X / (255 * 255 * 255)
-X = tf.stack(X)
-y = np.array(y)
-y = tf.stack(y)
 
-print('shape: ', X.shape, y.shape)
 
-POOL_SIZES = [i+1 for i in range(10)]
-STRIDE_SIZES = [i+1 for i in range(10)]
-KERNEL_SIZES = [i+1 for i in range(10)]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+y_train = np.array(y_train)
+
+y_train = to_categorical(y_train, 3)
+y_test = to_categorical(y_test, 3)
+#print('Shape: ', X.shape[0])
+print('Train Shape: ', X_train.shape)
+# print(X.size)
+# print(X.ndim)
+
+# X.reshape((X.shape[0], -1))
+X_train = np.array(X_train).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+X_train = X_train / (255 * 255 * 255)
+#X = tf.stack(X)
+
+
+
+POOL_SIZES = [i + 1 for i in range(10)]
+STRIDE_SIZES = [i + 1 for i in range(10)]
+KERNEL_SIZES = [i + 1 for i in range(10)]
 # how many dense layers we want to have
 dense_layers = [0, 1, 2]
 layer_sizes = [32, 64, 128]
 # how many conv layers do we want to have
 conv_layers = [3, 4, 5, 6]
 
-learn_rates = [0.001, 0.003, 0.005]#, 0.01, 0.03, 0.05, 0.1, 0.3, 0.5]
+learn_rates = [0.001, 0.003, 0.005]  # , 0.01, 0.03, 0.05, 0.1, 0.3, 0.5]
 dropout_rates = [0.1, 0.2, 0.3]
 filters = [str(8)]
 
 active_functions = ['softmax', 'tanh', 'sigmoid']
+
+#
+#GETTING RID OF VALIDATION AND ADDING SPLITTING THE DATASET BEFORE THE FIT METHOD
+#
+#
 
 for dense_layer in dense_layers:
     for layer_size in layer_sizes:
@@ -83,11 +105,12 @@ for dense_layer in dense_layers:
                                             current_time = now.strftime('%H_%M_%S')
                                             NAME = f'{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{learn_rate}-learn rate-{pool_size}-pool-{stride_size}-stride-{kernel_size}-kernel-{dropout}-dropout-{current_time}'
                                             tb = TensorBoard(log_dir=f'logs/{NAME}')
+                                            optimize = Adam(learning_rate=learn_rate)
                                             print(NAME)
 
                                             model = Sequential()
                                             model.add(layers.Convolution2D(64, (kernel_size, kernel_size),
-                                                                           input_shape=X.shape[1:], activation='relu'))
+                                                                           input_shape=X_train.shape[1:], activation='relu'))
                                             model.add(layers.MaxPooling2D(pool_size=(pool_size, pool_size),
                                                                           strides=(stride_size, stride_size)))
 
@@ -101,19 +124,21 @@ for dense_layer in dense_layers:
                                             model.add(layers.Flatten())
                                             for l in range(dense_layer):
                                                 model.add(layers.Dense(layer_size, activation='relu'))
-
                                             model.add(layers.Dropout(dropout))
-                                            model.add(layers.Dense(1, activation=each_function))
-                                            optimize = Adam(learning_rate=learn_rate)
+                                            model.add(layers.Dense(3, activation=each_function))
 
                                             # Train
-                                            model.compile(loss=binary_crossentropy, optimizer=optimize,
+                                            model.compile(loss=categorical_crossentropy, optimizer=optimize,
                                                           metrics=['accuracy'])
-                                            model.fit(X, y, batch_size=32, epochs=30, validation_split=0.3, callbacks=[tb])
+                                            model.fit(X_train, y_train, batch_size=32, epochs=30,
+                                                      callbacks=[tb], validation_data=(X_test, y_test))
                                         except Exception as e:
                                             print('exception')
                                             print(e.with_traceback())
+                                            print(e.__traceback__)
                                             pass
+
+
 
 """
 Test Data Results
@@ -121,7 +146,13 @@ Test Data Results
 3-conv-64-nodes-2-dense-1649025778: 29% accuracy
 
 """
+"""
+Checkpoint
+3-conv-32-nodes-0-dense-0.001-learn rate-2-pool-7-stride-8-kernel-0.1-dropout-20_53_01
 
+
+
+"""
 """
 train_ds = tf.keras.utils.image_dataset_from_directory(
     directory='big_data/training_images/',
